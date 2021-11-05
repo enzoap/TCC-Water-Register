271c451105483706d7a2d1f4e024927927a80a78
Lucas Okabe Simoncelio <simoncelio1965@gmail.com>
2021-11-04 23:44:39 -0300

    Atualização da tela principal 2.0

--- app/src/main/java/com/unip/tcc/welcome/presentation/WelcomeViewModel.kt
+++ app/src/main/java/com/unip/tcc/welcome/presentation/WelcomeViewModel.kt
@@ -1,8 +1,19 @@
 package com.unip.tcc.welcome.presentation
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.unip.tcc.welcome.domain.usecase.GetWaterConsumption
+import kotlinx.coroutines.launch
 
-class WelcomeViewModel() : ViewModel() {
+class WelcomeViewModel(
+    private val getWaterConsumption: GetWaterConsumption
+    ) : ViewModel() {
+    init {
 
-
+    }
+    private fun getInfo (){
+        viewModelScope.launch {
+            getWaterConsumption()
+        }
+    }
 }
